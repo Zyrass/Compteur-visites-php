f10bc26de3c27219b41adbd443b2b4a5d7cbdada
Zyrass <zyrass@outlook.fr>
2016-11-14 11:21:38 +0100

    Ajout d'un test avec l'invite de laragon

--- compteur.php
+++ compteur.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+    <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
@@ -14,6 +14,6 @@
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        <h1>Insertion en cours de traitement</h1>
+        <h1>Insertion en cours de traitement teste</h1>
     </body>
 </html>
